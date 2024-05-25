c6fe676824dd90b1a6ceb01969c96892dbeaf165
kazblacktopp <karen.blacktopp@gmail.com>
2024-05-25 21:54:56 +1000

    Add menu items and selection logic

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,38 @@
-export default function SideBar() {
+export default function SideBar({ onMenuSelection, selection }) {
+	const menuItems = ['Income', 'Expenses', 'Budget', 'Savings'];
+
+	let menuItemHoverStyles = 'hover:font-medium hover:text-slate-800';
+
 	return (
-		<div className="w-1/5 h-full bg-slate-400">
-			<h2>Menu</h2>
+		<div className="flex h-full w-1/5 flex-col justify-between rounded-e-3xl bg-blue-300 text-center font-display">
+			<h1
+				className="mb-auto mt-6 cursor-pointer px-4 text-2xl font-medium text-slate-700 hover:text-slate-800"
+				onClick={() => onMenuSelection(undefined)}
+			>
+				Ultimate Budget
+			</h1>
+			<ul className="my-auto flex flex-col gap-6 text-xl text-slate-600">
+				{menuItems.map((item, index) => {
+					return (
+						<li
+							key={index}
+							className={`cursor-pointer ${
+								selection === item
+									? 'font-medium text-slate-800'
+									: menuItemHoverStyles
+							}`}
+							onClick={() => onMenuSelection(item)}
+						>
+							{item}
+						</li>
+					);
+				})}
+			</ul>
+			<div
+				className={`mb-8 mt-auto cursor-pointer text-slate-600 ${menuItemHoverStyles}`}
+			>
+				Log Out
+			</div>
 		</div>
 	);
 }
