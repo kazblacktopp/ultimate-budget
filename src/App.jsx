5bf069ea6a721d7c96a0fd6e8c4e3fe579bcf27e
kazblacktopp <karen.blacktopp@gmail.com>
2024-05-25 21:56:05 +1000

    Add logic to handle menu selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+import { useState } from 'react';
 import SideBar from './components/SideBar';
 
 export default function App() {
+	const [selectedPage, setSelectedPage] = useState(undefined);
+
+	function handleMenuItemSelection(menuSelection) {
+		setSelectedPage(menuSelection);
+	}
+
+	let content = (
+		<h2 className="mt-4 text-center font-display text-2xl">
+			{selectedPage}
+		</h2>
+	);
+
+	if (selectedPage === undefined) {
+		content = (
+			<h2 className="mt-4 text-center font-display text-2xl">
+				Welcome to Ultimate Budget
+			</h2>
+		);
+	}
+
 	return (
-		<div className="flex w-screen h-screen">
-			<SideBar />
-			<main>
-				<h2>Main Window</h2>
-			</main>
+		<div className="flex h-screen w-screen">
+			<SideBar
+				onMenuSelection={handleMenuItemSelection}
+				selection={selectedPage}
+			/>
+			<main className="w-4/5">{content}</main>
 		</div>
 	);
 }
