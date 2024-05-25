0d97760bcb5274977c32f772ae30285b4c1b29ff
kazblacktopp <karen.blacktopp@gmail.com>
2024-05-25 19:36:27 +1000

    Add google font to html file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			// Adds a new `font-display` class
+			fontFamily: {
+				display: '"Roboto Condensed", ui-sans-serif',
+			},
+		},
 	},
 	plugins: [],
 };
