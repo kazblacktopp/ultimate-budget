72fc5c3550f5755d1ea3630bdf798823764c322d
kazblacktopp <karen.blacktopp@gmail.com>
2024-05-25 13:26:12 +1000

    Create SideBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import SideBar from './components/SideBar';
+
 export default function App() {
-	return <div></div>;
+	return (
+		<div className="flex w-screen h-screen">
+			<SideBar />
+			<main>
+				<h2>Main Window</h2>
+			</main>
+		</div>
+	);
 }
 
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,7 @@
+export default function SideBar() {
+	return (
+		<div className="w-1/5 h-full bg-slate-400">
+			<h2>Menu</h2>
+		</div>
+	);
+}
