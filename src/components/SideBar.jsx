4e662f79ed546068fb014e6c8da3b93cd6940e54
kazblacktopp <karen.blacktopp@gmail.com>
2024-05-27 21:20:58 +1000

    Update width style to flex basis

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,7 @@ export default function SideBar({ menuItems, onMenuSelection, selection }) {
 	let menuItemHoverStyles = 'hover:font-medium hover:text-slate-800';
 
 	return (
-		<div className="flex h-full w-1/5 flex-col justify-between rounded-e-3xl bg-blue-300 text-center font-display">
+		<div className="flex h-full basis-1/5 flex-col justify-between rounded-e-3xl bg-blue-300 text-center font-display">
 			<h1
 				className="mb-auto mt-6 cursor-pointer px-4 text-2xl font-medium text-slate-700 hover:text-slate-800"
 				onClick={() => onMenuSelection(null)}
